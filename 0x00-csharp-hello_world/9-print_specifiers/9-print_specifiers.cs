54da401a07eb52fcc49b32fd05061512a1a73833
carlosariasf <733@holbertonschool.com>
2020-04-13 19:28:32 -0500

    Task 9 fix

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 	    double percent = .7553;
 	    double currency = 98765.4321;
-        Console.WriteLine("{0:P}", percent);
-        Console.WriteLine("{0:C2}", currency);
+        Console.WriteLine("Percent: {0:P}", percent);
+        Console.WriteLine("Currency: {0:C2}", currency);
 	}
 }
