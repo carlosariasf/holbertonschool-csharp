c66414722d0cb3eb21b8a6ba67141d3bcf2634a8
carlosariasf <733@holbertonschool.com>
2020-04-13 19:01:36 -0500

    Task 8 test?

--- 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -5,8 +5,8 @@ class Program
     static void Main(string[] args)
     {
         string str1 = "Street";
-        string str2 = "Mission";
-        int int1 = 972;
+	    string str2 = "Mission";
+	    int int1 = 972;
         Console.WriteLine(int1 + " " + str2 + " " + str1);
     }
 }
