688450a6e8050cc39e50d35c749b5e8f4553b3c5
carlosariasf <733@holbertonschool.com>
2020-04-13 19:10:39 -0500

    Task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+	    double percent = .7553;
+	    double currency = 98765.4321;
+        Console.WriteLine("{0:P}", percent);
+        Console.WriteLine("{0:C2}", currency);
+	}
+}
